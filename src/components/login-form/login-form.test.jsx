26fd2f9591ab1308ee44aa53d5350ab1f0bb17c7
Liam Gallagher <gallaliamj@gmail.com>
2021-11-23 16:04:20 -0600

    added some tests

---
+++ src/components/login-form/login-form.test.jsx
@@ -0,0 +1,13 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import LoginForm from './login-form';
+
+test('Renders', () => {
+  render(<LoginForm />);
+
+  screen.getByText('SendSafely package browser login');
+  screen.getByText('Username:');
+  screen.getByText('Password:');
+});
---
+++ src/components/package-history-header/package-history-header.test.jsx
@@ -0,0 +1,31 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import PackageHistoryHeader from './package-history-header';
+
+test('Renders', () => {
+  const userData = { email: 'testEmail' };
+
+  render(<PackageHistoryHeader userData={userData} />);
+
+  screen.getByText('SendSafely package browser');
+  screen.getByText(userData.email);
+  screen.getByText('Logout');
+});
+
+test('Logout sets user data to null', () => {
+  const userData = { email: 'testEmail' };
+
+  const mockSetUserData = jest.fn();
+
+  render(
+    <PackageHistoryHeader userData={userData} setUserData={mockSetUserData} />
+  );
+
+  const logoutButtonComponent = screen.getByText('Logout');
+
+  fireEvent.click(logoutButtonComponent);
+
+  expect(mockSetUserData.mock.calls[0][0]).toBeNull();
+});
